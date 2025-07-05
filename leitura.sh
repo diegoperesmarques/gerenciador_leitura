330a4dacc0d8de43710f20f9abec666ac1bae2e0
diegoperesmarques@gmail.com <102305401+perescmexx@users.noreply.github.com>
2025-07-05 20:11:11 -0300

    FEAT: iniciando cadastro livros

--- leitura.sh
+++ leitura.sh
@@ -2,10 +2,59 @@
 
 source ./funcoes_uteis.sh
 
+function menu_livros() {
+    local -a OPCOES_MENU_PRINCIPAL=(
+        "1" "Listar livros"
+        "2" "Cadastrar livros"
+        "3" "Editar livros"
+        "4" "Excluir livros"
+    )
+
+    opcao_selecionada=$(menu_opcoes "Gerenciamento de livros" "${OPCOES_MENU_PRINCIPAL[@]}")
+
+    case $opcao_selecionada in 
+        1) 
+            listar_livros
+        ;;
+        2) 
+            cadastrar_livros
+        ;;
+        3) 
+            editar_livros
+        ;;
+        *)
+            excluir_livros
+        ;;
+    esac
+
+}
+
+function listar_livros() 
+{
+    echo "Chegou na função listar livros"
+}
+
+function cadastrar_livros() 
+{
+    echo "Chegou na função cadastrar livros"
+}
+
+function editar_livros() 
+{
+    echo "Chegou na função editar livros"
+}
+
+function excluir_livros() 
+{
+    echo "Chegou na função excluir livros"
+}
+
+
+
 
 function principal() {
 
-    declare -a OPCOES_MENU_PRINCIPAL=(
+    local -a OPCOES_MENU_PRINCIPAL=(
         "1" "Livros"
         "2" "Histórico de leitura"
     )
@@ -15,8 +64,7 @@ function principal() {
 
     case $opcao_selecionada in
     1)
-        imprimir_titulo "Você está na opção livros"
-        echo "Você escolheu a opção 1: Livros."
+        menu_livros
         ;;
     2)
         imprimir_titulo "Você está na opção histórico de leitura"
